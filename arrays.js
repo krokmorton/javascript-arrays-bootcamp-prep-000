45efc78e793173c59477b5d53f4398f04887dd37
Katy Morton <krokmorton@gmail.com>
2019-07-29 03:02:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,7 @@ function destructivelyAddElementToEndOfArray(chocolateBars, element){
 function accessElementInArray(chocolateBars, index){
   return chocolateBars[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars, element){
+  chocolateBars.sh
+}
